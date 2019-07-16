a4917ecd85bd5c5afd7a88cf486acee237e8d00a
Zachary M Waxman <zwaxman@gmail.com>
2019-07-16 21:37:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string.toLowerCase()===str){
+  if(string.toLowerCase()===string){
     return "I can\'t hear you!"
   }
-  else if(string.toUpperCase()===str){
+  else if(string.toUpperCase()===string){
     return "YES INDEED!"
   }
   else if(string==="I love you, Grandma."){
